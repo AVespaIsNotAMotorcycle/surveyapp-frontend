1f1b6e9b6c5244431ef857a0bda838a53a954d81
sam.ebersole@groenewold-it.solutions <samuelmebersole@gmail.com>
2023-07-26 21:00:48 +0200

    Surveys components now requests backend for survey list

--- src/components/Surveys.jsx
+++ src/components/Surveys.jsx
@@ -1,13 +1,47 @@
-import React, { useContext, useState } from 'react';
+import React, {
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import axios from 'axios';
 
 import UserContext from '../UserContext';
+import InlineMessage from './InlineMessage';
 
 function Surveys() {
   const { token } = useContext(UserContext);
-  const [surveys] = useState([]);
+  const [surveys, setSurveys] = useState([]);
+  const [inlineMessages, setInlineMessages] = useState([]);
+
+  useEffect(() => {
+    if (!token) return;
+    axios.get(
+      'http://localhost:8000/surveys',
+      { headers: { authorization: token } },
+    )
+      .then(({ data }) => { setSurveys(data); })
+      .catch(({ message }) => {
+        inlineMessages.push({
+          key: Date.now(),
+          type: 'error',
+          message,
+        });
+        setInlineMessages([...inlineMessages]);
+      });
+  }, [token]);
+
   return (
     <div>
-      {token}
+      {inlineMessages.map(({ key, type, message }) => (
+        <InlineMessage key={key} type={type}>
+          {message}
+        </InlineMessage>
+      ))}
+      {surveys.length === 0 && (
+        <InlineMessage type="success">
+          There are no surveys available.
+        </InlineMessage>
+      )}
       {surveys}
     </div>
   );
