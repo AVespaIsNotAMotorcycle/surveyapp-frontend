20630449edd55ddc4a6f025a278ebf48a0fdfbca
sam.ebersole@groenewold-it.solutions <samuelmebersole@gmail.com>
2023-07-26 21:52:18 +0200

    Sign In button only displays when not signed in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import UserContext from '../UserContext';
+import routes from '../routes';
+
 function Navbar() {
+  const { token } = useContext(UserContext);
+
   return (
     <nav>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/sign-in">Sign In</Link></li>
-        <li><Link to="/surveys">Surveys</Link></li>
+        {!token && (<li><Link to={routes.signIn}>Sign In</Link></li>)}
+        <li><Link to={routes.surveys}>Surveys</Link></li>
       </ul>
     </nav>
   );
